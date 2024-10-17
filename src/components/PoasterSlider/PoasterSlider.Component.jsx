6cbe8adfb7bf294965b7c769d964c8be77886d83
ChethanTN7 <chethantn947@gmail.com>
2024-10-17 16:39:34 +0530

    done with navbar

--- src/components/PoasterSlider/PoasterSlider.Component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PoasterSlider = () => {
-  return (
-    <div>PoasterSlider</div>
-  )
-}
-
-export default PoasterSlider
--- src/pages/Home.Page.jsx
+++ src/pages/Home.Page.jsx
@@ -4,7 +4,7 @@ import DefaultLayoutHoc from "../layouts/Default.Layout";
 // components
 import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.Component";
 import HeroCarousel from "../components/HeroCarousel/HeroCarousel.Component";
-import PoasterSlider from "../components/PoasterSlider/PoasterSlider.Component";
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
 
 const HomePage = () => {
   const [recommondedMovies, setRecommondedMovies] = useState([]);
@@ -14,7 +14,49 @@ const HomePage = () => {
   return (
     <>
       <HeroCarousel />
-      <div></div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 my-3">
+          The Best Of Entertainment
+        </h1>
+        <EntertainmentCardSlider />
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
+          title="Recommended Movies"
+          subtitle="List of rocommended movies"
+          posters={recommondedMovies}
+          isDark={false}
+        />
+      </div>
+
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className="hidden md:flex">
+            <img
+              src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+              alt="Rupay"
+              className="w-full h-full"
+            />
+          </div>
+          <PosterSlider
+            title="Premiers"
+            subtitle="Brand New Releases Every Friday"
+            posters={premierMovies}
+            isDark={true}
+          />
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+        <PosterSlider
+          title="Online Streaming Events"
+          subtitle="Online Stream Events"
+          posters={onlineStreamEvents}
+          isDark={false}
+        />
+      </div>
     </>
   );
 };
